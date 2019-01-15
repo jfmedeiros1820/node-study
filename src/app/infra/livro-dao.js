be5e544601ef2eb503f9078d33e07bbd04a73a70
João Felipe Medeiros Moreira <joaomedeiros@Joaos-MacBook-Pro-2.local>
2019-01-14 22:49:19 -0200

    Implements middlewares

--- src/app/infra/livro-dao.js
+++ src/app/infra/livro-dao.js
@@ -16,6 +16,32 @@ class LivroDao {
       )
     });
   }
+
+  add({ titulo, preco, descricao }) {
+    return new Promise((resolve, reject) => {
+      this._db.run(`
+        INSERT INTO livros (
+          titulo,
+          preco,
+          descricao
+        ) values (?, ?, ?)
+        `,
+        [
+          titulo,
+          preco,
+          descricao
+        ],
+        error => {
+          if(error) {
+            console.log(error);
+            return reject('Não foi possível adicionar o livro!');
+          }
+
+          resolve();
+        }
+      )
+    })
+  }
 }
 
 module.exports = LivroDao;
--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -25,4 +25,16 @@ module.exports = app => {
       ))
       .catch(erro => console.log(erro));
   });
+
+  app.get('/livros/form', function(req, resp) {
+    resp.marko(require('../views/livros/form/form.marko'));
+  });
+
+  app.post('/livros', function(req, resp) {
+    console.log(req.body);
+    const livroDao = new LivroDao(db);
+    livroDao.add(req.body)
+      .then(resp.redirect('/livros'))
+      .catch(erro => console.log(erro));
+  });
 }
